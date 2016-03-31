e5c9c246c1911e57b6345040f38353271d13b697
cagryinside <cagricetin89@gmail.com>
2016-03-30 23:53:45 -0400

    fix port number

--- adbconnect.sh
+++ adbconnect.sh
@@ -4,4 +4,4 @@ adb tcpip $PORT
 sleep 2
 IP=$(adb shell ifconfig | grep 'broadcast\|Bcast' | awk -F ' ' {'print $2'} | head -n 1 | sed -e 's/addr://g')
 ADDR="$IP:$PORT"
-adb connect $IP:5555
+adb connect $ADDR
